cfe8ff0a73dde2622bd8e6126c3d06091b7324c5
Philser <Pk30.05@hotmail.de>
2022-01-29 19:46:12 +0100

    Solve challenge

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-const product = 'Socks'
+let product = 'Boots'
+let description = 'I was told to add this here'
+
+const app = Vue.createApp({
+    data() {
+        return {
+            product,
+            description
+        }
+    }
+})
+
